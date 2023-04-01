f8b61861f86263553dcd09fb8afd7a7854317f31
Ivan Kopytin <i.ivanzemsky2005@gmail.com>
2023-04-01 22:22:53 +0300

    start

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+   return (
+      <header>
+         <div>
+            <span className="logo">House Staff</span>
+            <div className="presentation"></div>
+         </div>
+      </header>
+   );
+}
