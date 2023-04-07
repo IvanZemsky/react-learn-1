aac4bc75b2e55b933c97df37a36f99e949b82668
Ivan Kopytin <i.ivanzemsky2005@gmail.com>
2023-04-07 22:11:58 +0300

    cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useState } from "react";
+import cartIcon from './../img/basket.png';
 
 export default function Header() {
+   let [cartOpen, setCartOpen] = useState(false);
+
    return (
       <header>
          <div>
@@ -10,6 +14,14 @@ export default function Header() {
                <li>Конатакты</li>
                <li>Кабинет</li>
             </ul>
+            <img
+               onClick={() => setCartOpen(cartOpen = !cartOpen)}
+               className={`shop-cart-button ${cartOpen && 'active'}`}
+               src={cartIcon} alt="cart" width="30" height="30"
+            />
+            {cartOpen && (
+               <div className="shop-cart"></div>
+            )}
          </div>
          <div className="presentation"></div>
       </header>
